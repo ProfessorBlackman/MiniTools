624e4c85f4ae35b3ca8783cd7faf60667afb3e11
Methuselah-Nwodobeh <methuselah.nwodobeh@amalitech.com>
2023-08-29 12:09:17 +0000

    feat: done with url shortener

--- apps/rapidqr/services/utilities/QRCodeUtils.py
+++ apps/rapidqr/services/utilities/QRCodeUtils.py
@@ -7,22 +7,17 @@ from apps.rapidqr.models.qr_code_model import QRCode
 
 
 class QRCodeService:
+    qr_code = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
     def __init__(self, data):
         self.data = data
 
-    def generate_qrcode(self):
-        qr = qrcode.QRCode(
-            version=1,
-            error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=10,
-            border=4,
-        )
-        qr.add_data(self.data)
-        qr.make(fit=True)
-
-        qr_image = qr.make_image(fill_color="black", back_color="white")
-        return qr_image
-
+    def generate_qrcode_with_image_at_the_center(self):
+        pass
     def save_qrcode(self):
         qr_image = self.generate_qrcode()
 
@@ -32,3 +27,11 @@ class QRCodeService:
 
         qrcode_instance = QRCode.objects.create(qr_code=qr_file)
         return qrcode_instance
+
+    def generate_qrcode(self):
+        self.qr_code.add_data(self.data)
+        self.qr_code.make(fit=True)
+
+        qr_image = self.qr_code.make_image(fill_color="black", back_color="white")
+        return qr_image
+        pass
